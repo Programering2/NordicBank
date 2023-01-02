4905893a9d084e11b69c5e064d56c23fdf84ba7b
Vickeviking <92083048+Vickeviking@users.noreply.github.com>
2023-01-02 20:51:12 +0100

    komenterat

--- BankClassLibrary/Account.cs
+++ BankClassLibrary/Account.cs
@@ -15,46 +15,46 @@ namespace BankClassLibrary
         Banken myBank = new Banken();
         //List()<Trans>
 
-        public Account(Banken bank)
+        public Account(Banken bank) //konstruktor för Account 
         {
             this.balance = 100.00f;
             myBank = bank;
             Random random = new Random();
-            BankNumber = random.Next(100000, 999999);
+            BankNumber = random.Next(100000, 999999); //här initialiserar vi alla fields
         }
 
         //Add trans();
         //sort history()
 
         //getters
-        public float getBalance()
+        public float getBalance() //getter för balance
         {
             return balance;
         }
 
-        public float getBankNumber()
+        public float getBankNumber() //getter för banknummer
         {
             return BankNumber;
         }
 
         //setters
-        private protected void setBankNumber(int number)
+        private protected void setBankNumber(int number) //setter för banknummer
         { 
             this.BankNumber = number;
         }
 
-        public void setBalance(int balance)
+        public void setBalance(int balance) //setter för balance
         { 
             this.balance = balance;
         
         }
 
-        public void AddBalance(float balance)
+        public void AddBalance(float balance) // adera balancen
         {
             this.balance += balance;
         }
 
-        public void subtractBalance(float balance)
+        public void subtractBalance(float balance) //subtrahera balance
         {
             this.balance -= balance;
         }
